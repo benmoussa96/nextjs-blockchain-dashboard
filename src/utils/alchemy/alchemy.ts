83b630dc8500ba6f47d78133b47a6929e21566bf
ghaieth96 <ghaiethbmoussa@gmail.com>
2024-04-10 21:57:30 +0100

    build: enviorment setup

--- src/utils/alchemy/alchemy.ts
+++ src/utils/alchemy/alchemy.ts
@@ -3,20 +3,18 @@ import { Network } from 'alchemy-sdk';
 import { AlchemyMultichainClient } from './alchemy-multichain-client';
 
 export function buildAlchemyClient(apiKey?: string) {
-  if (apiKey) {
-    // Default config to use for all networks.
-    const defaultConfig = {
-      apiKey,
-      network: Network.ETH_MAINNET,
-    };
+  // Default config to use for all networks.
+  const defaultConfig = {
+    apiKey,
+    network: Network.ETH_MAINNET,
+  };
 
-    // Include optional setting overrides for specific networks.
-    const overrides = {
-      [Network.MATIC_MAINNET]: { apiKey },
-      [Network.ARB_MAINNET]: { apiKey },
-      [Network.OPT_MAINNET]: { apiKey },
-    };
+  // Include optional setting overrides for specific networks.
+  const overrides = {
+    [Network.MATIC_MAINNET]: { apiKey },
+    [Network.ARB_MAINNET]: { apiKey },
+    [Network.OPT_MAINNET]: { apiKey },
+  };
 
-    return new AlchemyMultichainClient(defaultConfig, overrides);
-  }
+  return new AlchemyMultichainClient(defaultConfig, overrides);
 }
