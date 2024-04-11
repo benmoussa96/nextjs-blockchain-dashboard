cbeb6e30d42de03e13a92ccef4b70717355d0d3f
ghaieth96 <ghaiethbmoussa@gmail.com>
2024-04-11 01:27:28 +0100

    feat: adding header

--- src/layouts/app-layout.tsx
+++ src/layouts/app-layout.tsx
@@ -2,6 +2,7 @@
 
 // import Header from '@/layouts/app-header';
 import { useSidebars } from '@/layouts/app-layout-utils';
+import Header from '@/layouts/header';
 import LeftSidebarFixed from '@/layouts/sidebar/left-sidebar-fixed';
 import SidebarExpandable from '@/layouts/sidebar/sidebar-expanded';
 import cn from '@/utils/class-names';
@@ -14,6 +15,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
       <LeftSidebarFixed />
       <SidebarExpandable />
       <div className="flex w-full flex-col ">
+        <Header className="xl:ms-[88px]" />
         <div
           className={cn(
             'flex flex-grow flex-col gap-4 px-4 pb-6 duration-200 md:px-5 lg:pb-8  xl:pe-8 ',
---
+++ src/layouts/header/index.tsx
@@ -0,0 +1,101 @@
+'use client';
+
+import Logo from '@/components/logo';
+import HamburgerButton from '@/layouts/header/hamburger-button';
+import MessagesDropdown from '@/layouts/header/messages-dropdown';
+import NotificationDropdown from '@/layouts/header/notification-dropdown';
+import ProfileMenu from '@/layouts/header/profile-menu';
+import StickyHeader from '@/layouts/header/sticky-header';
+import Sidebar from '@/layouts/sidebar/sidebar-drawer';
+import cn from '@/utils/class-names';
+import Link from 'next/link';
+import {
+  PiBellSimpleRingingDuotone,
+  PiChatsCircleDuotone,
+} from 'react-icons/pi';
+import { ActionIcon, Badge } from 'rizzui';
+
+function HeaderMenuRight() {
+  return (
+    <div className="ms-auto flex shrink-0 items-center gap-2 text-gray-700 xs:gap-3 xl:gap-4">
+      <MessagesDropdown>
+        <ActionIcon
+          aria-label="Messages"
+          variant="text"
+          className={cn(
+            ' relative h-[34px] w-[34px] overflow-hidden rounded-full md:h-9 md:w-9 3xl:h-10 3xl:w-10 '
+          )}
+        >
+          <PiChatsCircleDuotone className="h-6 w-auto" />
+          <Badge
+            renderAsDot
+            color="success"
+            enableOutlineRing
+            className="absolute right-1 top-2.5 -translate-x-1 -translate-y-1/4"
+          />
+        </ActionIcon>
+      </MessagesDropdown>
+      <NotificationDropdown>
+        <ActionIcon
+          aria-label="Notification"
+          variant="text"
+          className={cn(
+            'relative h-[34px] w-[34px] overflow-hidden rounded-full md:h-9 md:w-9 3xl:h-10 3xl:w-10'
+          )}
+        >
+          <PiBellSimpleRingingDuotone className="h-6 w-auto" />
+          <Badge
+            renderAsDot
+            color="warning"
+            enableOutlineRing
+            className="absolute right-1 top-2.5 -translate-x-1 -translate-y-1/4"
+          />
+        </ActionIcon>
+      </NotificationDropdown>
+      <ProfileMenu
+        buttonClassName="w-auto sm:w-auto p-1 border border-gray-300"
+        avatarClassName="!w-7 !h-7 sm:!h-8 sm:!w-8"
+      />
+    </div>
+  );
+}
+
+export default function Header({ className }: { className?: string }) {
+  return (
+    <StickyHeader
+      className={cn(
+        'justify-between bg-white xl:pe-8 dark:bg-gray-50/50',
+        className
+      )}
+    >
+      <div className="hidden items-center gap-3 xl:flex">
+        <Link
+          aria-label="Site Logo"
+          href={'/'}
+          className="me-4 hidden w-[155px] shrink-0 text-gray-800 hover:text-gray-900 lg:me-5 xl:block"
+        >
+          <Logo className="max-w-[155px] h-1/4" />
+        </Link>
+      </div>
+
+      <div className="flex w-full items-center justify-between gap-5 xl:w-[calc(100%_-_190px)] 2xl:w-[calc(100%_-_310px)] 3xl:gap-6">
+        <div className="flex max-w-2xl items-center xl:w-auto">
+          <HamburgerButton
+            view={<Sidebar className="static w-full 2xl:w-full" />}
+          />
+          <Link
+            aria-label="Site Logo"
+            href="/"
+            className="me-4 w-9 shrink-0 text-gray-800 hover:text-gray-900 lg:me-5 xl:hidden"
+          >
+            <Logo iconOnly={true} />
+          </Link>
+        </div>
+
+        <div className="flex items-center justify-between">
+          <HeaderMenuRight />
+        </div>
+      </div>
+    </StickyHeader>
+  );
+}
